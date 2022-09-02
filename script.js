902505725316d4b33df7226fc1fa981846caffd4
Harsha-kalava <harshcode16@gmail.com>
2022-09-02 11:45:52 +0530

    DOM Traversing

---
+++ script.js
@@ -0,0 +1,34 @@
+// ParentNode
+let itemList = document.querySelector("#items");
+// console.log(itemList.parentNode);
+
+// 1 - ParentElement
+// let itemList = document.querySelector("#items");
+// console.log(itemList.parentElement.parentElement);
+// itemList.parentElement.style.backgroundColor = "#f4f4f4";
+
+// 2 - lastelementchild
+// console.log(itemList.lastElementChild);
+
+// // 3 - lastchild
+// console.log(itemList.lastChild);
+
+// // 4 - createchild
+
+// // 5 - firstelementchild
+// console.log(itemList.firstElementChild);
+
+// // 6-firstchild
+// console.log(itemList.firstChild);
+
+// // 7- nextsibling
+// console.log(itemList.nextSibling);
+
+// // 8- nextelementsibling
+// console.log(itemList.nextElementSibling) //--give null
+
+// // 9- previoussibling
+// console.log(itemList.previousSibling)
+
+// // 10 - previouselementsibling
+// console.log(itemList.previouselementsibling);
